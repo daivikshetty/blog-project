a0ea6eb23bfe818dc8b4058ca1f58f7cfcd53474
Daivik Shetty <shettydaivik100@gmail.com>
2022-10-20 22:41:07 +0530

    added mongoose for mongoDB

--- app.js
+++ app.js
@@ -1,52 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const mySql = require('mysql');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+const userSchema = new mongoose.Schema({
+      username : String,
+      email : String,
+      password : String
+});                                             //create schema
+
+const User = mongoose.model("User", userSchema);
 
 const app = express();
-
-var con = mySql.createConnection({              // connect to mySQL server
-      host : "localhost",
-      user : "root",
-      password : "@daivik"
-});
-
-con.connect((err) => {
-      if(!err){
-            console.log("Connected!");
-
-            // var CREATE = "CREATE DATABASE testDB";          //creates testDB database
-            // con.query(CREATE, (err,result) => {
-            //       if(err){
-            //             console.log(err);
-            //       }
-            //       else{
-            //             console.log("Created Database");
-            //       }
-            // });
-
-            var CONTROL = "USE testDB";         //change to testDB
-            con.query(CONTROL, (err,result) => {
-                  if(err){
-                        console.log(err);
-                  }
-                  else{
-                        console.log("Using testDB");
-                  }
-            })
-
-            // var TABLE = "CREATE TABLE USERS(USERNAME VARCHAR(255), EMAIL VARCHAR(255), PASSWORD VARCHAR(255))";    //create table USERS
-            // con.query(TABLE, (err,result) => {
-            //       if(err){console.log(err);}
-            //       else{
-            //             console.log("Created Table");
-            //       }
-            // });
-      }
-      else{
-            console.log(err);
-      }
-});
 
 app.use(express.static("views"));
 app.set("view engine","ejs");
@@ -78,13 +45,11 @@ app.post("/login",(req,res) => {
             password : req.body.password
       };
 
-      const QUERRY = "SELECT * FROM users;";
+      console.log(details);
 
-      con.query(QUERRY, (err, result) => {
-            if(err){
-                  console.log(err);
-            }
-      });
+      //insert object here
+
+
       res.redirect("/login");
 });
 
@@ -95,25 +60,12 @@ app.post("/register",(req,res) => {
             password : req.body.password,
             confirmPassword : req.body.confirmPassword
       };
+      console.log(credentials);
 
-      if(credentials.password != credentials.confirmPassword){
-            console.log("Wrong password!!!");
-            res.redirect("/");
-      }
-      else{
-            var INSERT = "INSERT INTO USERS VALUES('" + credentials.userName + "','" + credentials.mailId + "','" + credentials.password +"')";
-            // console.log(INSERT);
-            // console.log(credentials);
-            con.query(INSERT, (err, result) => {
-                  if(err){
-                        console.log(err);
-                  }
-                  else{
-                        console.log("Inserted one value");
-                  }
-            });
-            res.redirect("/register");
-      }
+      const newUser = new User({username:credentials.userName,email:credentials.mailId,password:credentials.password});
+      newUser.save();
+
+      res.redirect("/register");
 });
 
 app.listen(3000,() => {
